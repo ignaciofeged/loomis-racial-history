22594c398b4581880dfdade3052751824b4f545c
ignaciofeged <109924349+ignaciofeged@users.noreply.github.com>
2022-07-31 23:30:08 -0400

    Add files via upload

---
+++ Loomis_init.R
@@ -0,0 +1,11 @@
+#init.R
+
+my_packages = c("tidyverse", "shinythemes", "timevis")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages())==FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages,install_if_missing))
