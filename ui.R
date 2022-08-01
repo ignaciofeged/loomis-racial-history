7468bd54905595163fc0158749f14972e05203a5
ignaciofeged <109924349+ignaciofeged@users.noreply.github.com>
2022-08-01 17:00:01 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ library(timevis)
 library(RCurl)
 
 tx<-getURL("https://raw.githubusercontent.com/ignaciofeged/loomis-racial-history/heroku/TimelineData4-1.csv")
-timeline_data1<-read_csv(text=tx)
+timeline_data1<-read_csv(tx)
 as.tibble(timeline_data1)
 timeline_data1$month[is.na(timeline_data1$month)]<-1
 timeline_data1$help<-timeline_data1$thing-1700
@@ -17,7 +17,7 @@ timeline_data1$help<-timeline_data1$help*365+timeline_data1$month*31
 timeline_data1$start<-as.Date(timeline_data1$help,origin="1700-01-01")
 
 ty<-getURL("https://raw.githubusercontent.com/ignaciofeged/loomis-racial-history/heroku/TimelineData4-2.csv")
-timeline_data2<-read_csv(text=ty)
+timeline_data2<-read_csv(ty)
 as.tibble(timeline_data2)
 timeline_data2$month[is.na(timeline_data2$month)]<-1
 timeline_data2$help<-timeline_data2$thing-1700
