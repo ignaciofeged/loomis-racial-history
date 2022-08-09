83172a573dabbe27759286879a958c86fbcc9acb
ignaciofeged <109924349+ignaciofeged@users.noreply.github.com>
2022-08-08 22:02:20 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,6 +1,38 @@
 #server.R
 
+library(readxl)
+library(tidyverse)
+library(shiny)
+library(shinythemes)
 library(timevis)
+library(RCurl)
+
+x<-getURL("https://raw.githubusercontent.com/ignaciofeged/loomis-racial-history/heroku/TimelineData4-1.csv")
+timeline_data1<-read_csv(x)
+as.tibble(timeline_data1)
+timeline_data1$month[is.na(timeline_data1$month)]<-1
+timeline_data1$help<-timeline_data1$thing-1700
+timeline_data1$help<-timeline_data1$help*365+timeline_data1$month*31
+timeline_data1$start<-as.Date(timeline_data1$help,origin="1700-01-01")
+
+y<-getURL("https://raw.githubusercontent.com/ignaciofeged/loomis-racial-history/heroku/TimelineData4-2.csv")
+timeline_data2<-read_csv(y)
+as.tibble(timeline_data2)
+timeline_data2$month[is.na(timeline_data2$month)]<-1
+timeline_data2$help<-timeline_data2$thing-1700
+timeline_data2$help<-timeline_data2$help*365+timeline_data2$month*31
+timeline_data2$start<-as.Date(timeline_data2$help,origin="1700-01-10")
+
+timeline_data<-rbind(timeline_data1,timeline_data2)
+as.tibble(timeline_data)
+as.factor(timeline_data$group)
+timeline_data$id<-1:121
+##Time Vis prep############
+timevisDataGroups <- data.frame(
+  id = c("CT", "SC", "USA"),
+  content = c("Connecticut", "South Carolina", "United States"))
+dday1<-as.Date("1698-01-01","%Y-%m-%d")
+dday2<-as.Date("1710-01-01","%Y-%m-%d")
 
 server <- function(input, output) {
   
