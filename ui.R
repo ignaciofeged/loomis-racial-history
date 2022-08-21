05e7654aef3b3848927fa073185ed3f4ebbe278a
ignaciofeged <109924349+ignaciofeged@users.noreply.github.com>
2022-08-20 23:03:13 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,4 @@
 #ui.R
-
-#find a way to reference the data in a way that works, check yt vids work and throw on github
 library(readxl)
 library(tidyverse)
 library(shiny)
@@ -15,36 +13,37 @@ dday1<-as.Date("1698-01-01","%Y-%m-%d")
 dday2<-as.Date("1710-01-01","%Y-%m-%d")
 
 ##Shiny UI#########
-ui<- fluidPage(tags$style(HTML(
-  ".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #98252b}")),#color goes here
-  theme = shinytheme("simplex"),
-  titlePanel("",windowTitle = "Historical Context: Timeline"),
-  titlePanel(
-    h1("Loomis' History of Slavery: In Context", align = "center")),
-  titlePanel(
-    h5("Click on the timeline events to get a description and adjust the years shown by using the slider, the play button, zooming in/out, or draging the timeline",align="center")),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        inputId = "date_range",
-        label = "Hit the Play Button or Drag the Slider to Change the Range of Years",
-        min = as.Date("1698-01-01","%Y-%m-%d"),
-        max = as.Date("1861-12-01","%Y-%m-%d"),
-        value = c(dday1,dday2),
-        timeFormat = "%Y-%m-%d",
-        step = 1095,
-        animate = 
-          animationOptions(
-            interval = 1500,
-            loop=F,)
-        
-      ),
-    ),
-    mainPanel(
-      timevisOutput("LCline"),
-      (conditionalPanel(condition = "output.show_notes", 
-                        h1("More Information:"),
-                        br())),
-      htmlOutput("printNotes")),
-  )
+ui<- fluidPage(theme = shinytheme("simplex"),
+               tags$head(
+                 tags$link(href = "style.css", rel = "stylesheet")),
+                titlePanel("",windowTitle = "Historical Context: Timeline"),
+                titlePanel(
+                  h1("Loomis' History of Slavery: In Context", align = "center")),
+                titlePanel(
+                  h5("Click on the timeline events to get a description and adjust the years shown by using the slider, the play button, zooming in/out, or draging the timeline",align="center")),
+                sidebarLayout(
+                  sidebarPanel(
+                    sliderInput(
+                      inputId = "date_range",
+                      label = "Hit the Play Button or Drag the Slider to Change the Range of Years",
+                      min = as.Date("1698-01-01","%Y-%m-%d"),
+                      max = as.Date("1861-12-01","%Y-%m-%d"),
+                      value = c(dday1,dday2),
+                      timeFormat = "%Y-%m-%d",
+                      step = 1095,
+                      animate = 
+                        animationOptions(
+                          interval = 1500,
+                          loop=F,)
+                      
+                    ),
+                  ),
+                  mainPanel(
+                    timevisOutput("LCline"),
+                    (conditionalPanel(condition = "output.show_notes", 
+                                      h1("More Information:"),
+                                      br())),
+                    htmlOutput("printNotes")),
+                )
+  
 )
